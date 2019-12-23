32894aa4141154df4ea6e9616e2883d124e35830
jwenjian <wenjian.jiang@foxmail.com>
2019-12-23 22:04:54 +0800

    change build output folder to docs

--- quasar.conf.js
+++ quasar.conf.js
@@ -55,6 +55,7 @@ module.exports = function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
+      distDir: 'docs',
       scopeHoisting: true,
       vueRouterMode: 'hash', // available values: 'hash', 'history'
       showProgress: true,
